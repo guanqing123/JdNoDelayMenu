59d373d6bb4cedd1bb34ea408e745da2abc1922f
gq <guanqinghz@163.com>
2017-09-24 23:14:06 +0800

    创建 README.md 文件

--- js/megadropdown.js
+++ js/megadropdown.js
@@ -10,11 +10,29 @@ $(document).ready(function(){
 	})
 	.on('mouseleave',function(e){
 		sub.addClass('none');
-		
 		if (activeRow) {
 			activeRow.removeClass('active');
 			activeRow = null;
 		}
-		
+		if (activeMenu) {
+			activeMenu.addClass('none');
+			activeMenu = null;
+		}
+	})
+	.on('mouseenter','li',function(e){
+		if (!activeRow) {
+			activeRow = $(e.target).addClass('active');
+			activeMenu = $('#' + activeRow.data('id'));
+			activeMenu.removeClass('none');
+			return;
+		}
+		// 把上一次激活的给不激活,同时不显示
+		activeRow.removeClass('active');
+		activeMenu.addClass('none');
+		// 把当前点击的设置为 激活,同时显示
+		activeRow = $(e.target);
+		activeRow.addClass('active');
+		activeMenu = $('#' + activeRow.data('id'));
+		activeMenu.removeClass('none');
 	});
 });
