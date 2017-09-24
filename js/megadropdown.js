a1d9a0be269b5c0c5b0749994001626a57ac1b98
gq <guanqinghz@163.com>
2017-09-24 23:52:26 +0800

    提交修改了 megadropdown.js

--- js/megadropdown.js
+++ js/megadropdown.js
@@ -4,9 +4,34 @@ $(document).ready(function(){
 	var activeRow;
 	var activeMenu;
 	
+	var timer;
+	
+	var mouseInSub = false; // 鼠标是否在二级菜单里面
+	sub.on('mouseenter',function(e){
+		mouseInSub = true;
+	})
+	.on('mouseleave',function(e){
+		mouseInSub = false;
+	});
+	
+	var mouseTrack = []; //创建鼠标,记录鼠标的位置
+	
+	var moveHandler = function(e) {
+		mouseTrack.push({
+			x: e.pageX,
+			y: e.pageY
+		});
+		//我们只要保存最新的位置 和 上一次的位置
+		if (mouseTrack.length > 3) {
+			mouseTrack.shift(); //多余的我们就把它弹出
+		}
+	};
+	
 	$('#test')
 	.on('mouseenter',function(e){
 		sub.removeClass('none');
+		
+		$(document).bind('mousemove',moveHandler); // 绑定
 	})
 	.on('mouseleave',function(e){
 		sub.addClass('none');
@@ -18,6 +43,8 @@ $(document).ready(function(){
 			activeMenu.addClass('none');
 			activeMenu = null;
 		}
+		
+		$(document).unbind('mousemove',moveHandler); // 解绑
 	})
 	.on('mouseenter','li',function(e){
 		if (!activeRow) {
@@ -26,13 +53,25 @@ $(document).ready(function(){
 			activeMenu.removeClass('none');
 			return;
 		}
-		// 把上一次激活的给不激活,同时不显示
-		activeRow.removeClass('active');
-		activeMenu.addClass('none');
-		// 把当前点击的设置为 激活,同时显示
-		activeRow = $(e.target);
-		activeRow.addClass('active');
-		activeMenu = $('#' + activeRow.data('id'));
-		activeMenu.removeClass('none');
+		
+		if (timer) { // debounce 去抖技术原理
+			clearTimeout(timer);
+		}
+		
+		timer = setTimeout(function() {
+			if (mouseInSub) {
+				return;
+			}
+			// 把上一次激活的给不激活,同时不显示
+			activeRow.removeClass('active');
+			activeMenu.addClass('none');
+			// 把当前点击的设置为 激活,同时显示
+			activeRow = $(e.target);
+			activeRow.addClass('active');
+			activeMenu = $('#' + activeRow.data('id'));
+			activeMenu.removeClass('none');
+			timer = null; // debounce 去抖技术原理
+		}, 300);
+		
 	});
 });
