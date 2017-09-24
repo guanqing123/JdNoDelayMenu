ea637a0abf776bf52f5156ade83917dbc08a34da
gq <guanqinghz@163.com>
2017-09-24 22:42:11 +0800

    导入 jquery 类库,创建 megadropdown.js 文件

---
+++ js/megadropdown.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	var sub = $('#sub');
+	
+	var activeRow;
+	var activeMenu;
+	
+	$('#test')
+	.on('mouseenter',function(e){
+		sub.removeClass('none');
+	})
+	.on('mouseleave',function(e){
+		sub.addClass('none');
+		
+		if (activeRow) {
+			activeRow.removeClass('active');
+			activeRow = null;
+		}
+		
+	});
+});
