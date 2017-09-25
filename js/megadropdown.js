b9ab850c37025f219a30e6d630ce726c26439a53
gq <guanqinghz@163.com>
2017-09-25 11:20:42 +0800

    增加一个 判断点是否在 三角形内的 function.js 文件

--- js/megadropdown.js
+++ js/megadropdown.js
@@ -58,20 +58,37 @@ $(document).ready(function(){
 			clearTimeout(timer);
 		}
 		
-		timer = setTimeout(function() {
-			if (mouseInSub) {
-				return;
-			}
-			// 把上一次激活的给不激活,同时不显示
-			activeRow.removeClass('active');
-			activeMenu.addClass('none');
-			// 把当前点击的设置为 激活,同时显示
-			activeRow = $(e.target);
-			activeRow.addClass('active');
-			activeMenu = $('#' + activeRow.data('id'));
-			activeMenu.removeClass('none');
-			timer = null; // debounce 去抖技术原理
-		}, 300);
+		var currMousePos = mouseTrack[mouseTrack.length - 1];
+		var leftCorner = mouseTrack[mouseTrack.length - 2];
 		
+		var delay = needDelay(sub, leftCorner, currMousePos);
+		if (delay) {
+			timer = setTimeout(function() {
+				if (mouseInSub) {
+					return;
+				}
+				// 把上一次激活的给不激活,同时不显示
+				activeRow.removeClass('active');
+				activeMenu.addClass('none');
+				// 把当前点击的设置为 激活,同时显示
+				activeRow = $(e.target);
+				activeRow.addClass('active');
+				activeMenu = $('#' + activeRow.data('id'));
+				activeMenu.removeClass('none');
+				timer = null; // debounce 去抖技术原理
+			}, 300);
+		} else{
+			var prevActiveRow = activeRow;
+			var prevActiveMenu = activeMenu;
+			
+			activeRow = $(e.target);
+			activeMenu = $('#' + activeRow.data('id'));
+			
+			prevActiveRow.removeClass('active');
+			prevActiveMenu.addClass('none');
+			
+			activeRow.addClass('active');
+			activeMenu.removeClass('none');
+		}	
 	});
 });
